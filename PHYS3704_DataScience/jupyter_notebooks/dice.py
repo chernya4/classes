5eed06f6a4282db0ab093504b67319ef67d38b66
Mykola Chernyashevskyy <chernya2@illinois.edu>
2025-02-02 23:02:31 -0500

    made Progress in Class

--- PHYS3704_DataScience/jupyter_notebooks/dice.py
+++ PHYS3704_DataScience/jupyter_notebooks/dice.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def rolldice(nsims):
 # nsims is number of simulations to do
     nsims=int(nsims)
-    prob=1/6.
+    prob=1/6. #this is a shortcut to say 1 out of 6
     is_one=(random.rand(nsims,100) < prob)
 # generate nsims sets of 100 rolls
     ndice_array=np.array([2,5,10,25,100])
@@ -13,7 +13,18 @@ def rolldice(nsims):
     for i,ndice in enumerate(ndice_array):
         plt.figure(i) # create a new figure for each plot
         plt.hist( np.sum(is_one[:,0:ndice],axis=1), 
-range=(-0.5,ndice+0.5),bins=(ndice + 1))
+        range=(-0.5,ndice+0.5),bins=(ndice + 1))
         plt.title(str(ndice) + ' dice')
         # convert ndice to a string with str(), then use that to title the plot
         plt.xlabel('Total number of ones')
+
+        print(f'ndice: {ndice}')
+        print(f'np.mean: {np.mean(np.sum(is_one[:,0:ndice],axis=1))}')
+        print(f'np.sum: {np.sum(np.sum(is_one[:,0:ndice],axis=1))/nsims:.4f}')
+        print(f'Expected mean: {ndice*prob:.4f} ')
+
+        print(f'np.std**2:{np.std( np.sum(is_one[:, 0:ndice],axis=1) )**2:.4f}')
+        print(f'np.var: {np.var( np.sum(is_one[:, 0:ndice],axis=1) ) :.4f}')
+        print(f'Expected variance: {ndice*prob*(1-prob):.4f}')
+        print('')
+    
