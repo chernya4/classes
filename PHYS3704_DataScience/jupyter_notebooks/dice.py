9216cc6828f5f195c877bc3ebad2b4af406309f7
Mykola Chernyashevskyy <chernya2@illinois.edu>
2025-02-03 16:07:17 -0500

    Did some Lecture 5 and Lecture 6 Work

--- PHYS3704_DataScience/jupyter_notebooks/dice.py
+++ PHYS3704_DataScience/jupyter_notebooks/dice.py
@@ -1,6 +1,8 @@
 import numpy.random as random
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats
+
 
 def rolldice(nsims):
 # nsims is number of simulations to do
@@ -11,11 +13,24 @@ def rolldice(nsims):
     ndice_array=np.array([2,5,10,25,100])
 
     for i,ndice in enumerate(ndice_array):
+
         plt.figure(i) # create a new figure for each plot
-        plt.hist( np.sum(is_one[:,0:ndice],axis=1), 
-        range=(-0.5,ndice+0.5),bins=(ndice + 1))
+
+        plt.hist(
+                 np.sum(is_one[:,0:ndice],axis=1), 
+                 range=(-0.5,ndice+0.5),
+                 bins=(ndice + 1)
+                )
+        
         plt.title(str(ndice) + ' dice')
+
+        # Compute expected binomial distribution
+        x = np.arange(0, ndice + 1) # generate an array representing all possible outcomes for the number of ones
+        binomial_pmf = scipy.stats.binom.pmf(x, ndice, prob) # Gives you NORMALIZED probability outcomes. 
+        expected_counts = nsims * binomial_pmf # Undoes the normalization to get the correct number of occurences.
+
         # convert ndice to a string with str(), then use that to title the plot
+        plt.ylabel('Number of occurences')
         plt.xlabel('Total number of ones')
 
         print(f'ndice: {ndice}')
